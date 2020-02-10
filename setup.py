abed7c842a29cffd26107f8e066b7f9ba6e32ee6
john21ro <john21ro@yahoo.com>
2020-02-10 16:42:26 +0200

    check for plugin translation

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/palcu/ckanext-datagovro_theme',
+    url='https://github.com/john21ro/ckanext-datagovro_theme',
 
     # Author details
     author='''Ionel David''',
