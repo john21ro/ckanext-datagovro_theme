bb6e4c3aabc77c6fe361433f7d8ad8a6839f035a
john21ro <john21ro@yahoo.com>
2020-05-23 22:10:08 +0300

    nomber of popular groups changed to 20

--- ckanext/datagovro_theme/plugin.py
+++ ckanext/datagovro_theme/plugin.py
@@ -24,13 +24,12 @@ def get_number_of_external_links():
 def most_popular_groups():
     '''Return a sorted list of the groups with the most datasets.'''
 
-    # Get a list of all the site's groups from CKAN, sorted by number of
-    # datasets.
+    # Get a list of all the site's groups from CKAN, sorted by number of datasets.
     groups = toolkit.get_action('group_list')(
         data_dict={'sort': 'package_count desc', 'all_fields': True})
 
-    # Truncate the list to the 10 most popular groups only.
-    groups = groups[:10]
+    # Truncate the list to the 20 most popular groups only.
+    groups = groups[:20]
 
     return groups
 
